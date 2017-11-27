45013ba53d9b6caada423ee14bc828035c0b6966
mananapr <manny011@live.com>
2017-11-27 15:27:52 +0530

    Added GUI for initial Main Menu

---
+++ src/application/Main.java
@@ -0,0 +1,31 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application{
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+		// FXMLs
+		Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+		
+		// SCENES
+		Scene main_scene = new Scene(root, 600, 400);
+		main_scene.getStylesheets().add(getClass().getResource("main.css").toExternalForm());
+		
+		// STAGE SETTINGS
+		primaryStage.setResizable(false);
+		primaryStage.setScene(main_scene);
+		primaryStage.show();
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
